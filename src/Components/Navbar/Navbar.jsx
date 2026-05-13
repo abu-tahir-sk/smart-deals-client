6cc2c36f66929da317e89a82d3cf1b1b620675cc
abu-tahir-sk <abutahir2626@gmail.com>
2026-05-13 04:47:51 -0900

    fix: navbar logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         {links}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">Smart <span className="text-purple-700">Deals</span></a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
